ae01706af2e4dc1cc71754ff04e664565c474cf1
v1p3r75 <elfriedv16@gmail.com>
2023-05-08 14:56:04 +0100

    Init Project

---
+++ _class/Student.py
@@ -0,0 +1,7 @@
+
+class Student:
+    def __init__(self):
+        self.start()
+
+    def start(self):
+        print('Start Student')
---
+++ main.py
@@ -0,0 +1,5 @@
+#!/bin/env python
+
+from _class.Student import Student
+
+student = Student()
